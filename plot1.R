caa5277f2a9e043c11d39780df011e5b0922cc5b
Summer Getty <c16summer.getty@usafa.edu>
2015-03-06 10:16:10 -0700

    1st commit of plots
    Exploratory Data Project 1

---
+++ plot1.R
@@ -0,0 +1,15 @@
+datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/exploratorydata/household_power_consumption.txt", header=TRUE, sep=';', na.strings="?", 
+                   nrows=2075259, check.names=FALSE, stringsAsFactors=FALSE, comment.char="", quote='\"')
+datacomp$Date<-as.Date(datacomp$Date, format="%d/%m/%Y")
+
+data<-subset(datacomp,subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(datacomp)
+
+datetime<-paste(as.Date(data$Date),data$Time)
+data$Datetime<-as.POSIXct(datetime)
+
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,15 @@
+datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/exploratorydata/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                   nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+datacomp$Date<-as.Date(datacomp$Date, format="%d/%m/%Y")
+
+subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(datacomp)
+
+datetime<-paste(as.Date(data$Date),data$Time)
+data$Datetime<-as.POSIXct(datetime)
+
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+dev.copy(png, file="plot2.png", height=480, width=480)
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
