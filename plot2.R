c7c070b2b50c5e6f124b1ef44c68de493eb814e6
Summer Getty <c16summer.getty@usafa.edu>
2015-04-10 22:04:39 -0600

    2nd commit

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
-datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/exploratorydata/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/Exploratory-Data-Project-1/exploratorydata/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                    nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 datacomp$Date<-as.Date(datacomp$Date, format="%d/%m/%Y")
 
-subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+subset=(datacomp$Date >= "2007-02-01" & datacomp$Date <= "2007-02-02")
 rm(datacomp)
 
 datetime<-paste(as.Date(data$Date),data$Time)
@@ -12,4 +12,5 @@ plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power
 
 dev.copy(png, file="plot2.png", height=480, width=480)
 plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,8 +1,8 @@
-datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/exploratorydata/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+datacomp<-read.csv("C:/Users/C16Summer.Getty/Documents/math378/Exploratory-Data-Project-1/exploratorydata/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                    nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 datacomp$Date<-as.Date(datacomp$Date, format="%d/%m/%Y")
 
-subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+subset=((Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(datacomp)
 
 datetime<-paste(as.Date(data$Date),data$Time)
@@ -20,14 +20,5 @@ with(data, {
 })
 
 dev.copy(png, file="plot4.png", height=480, width=480)
-par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,2,0))
-with(data, {
-  plot(Global_active_power~Datetime,type="l",ylab="Global Active Power (kilowatts)", xlab="")
-  plot(Voltage~Datetime,type="l",ylab="Voltage (volt)", xlab="")
-  plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~Datetime,type="l", ylab="Global Reactive Power (kilowatts)", xlab="")
-})
+
 dev.off()
